5e0ada58fdbb03939f6dc22c24cd5551f0579691
OleksandKur <136488587+OleksandrKur@users.noreply.github.com>
2024-10-07 15:24:41 +0300

    Add basic styles

--- src/app.js
+++ src/app.js
@@ -3,12 +3,13 @@ import {home} from "./home.js"
 import {menuPage} from "./menu.js"
 import {aboutUs} from "./about.js"
 
-home.generateContent()
 
 const homeButton = document.getElementById("home");
 const menuButton = document.getElementById("menu");
 const aboutButton = document.getElementById("about");
 
+
+home.generateContent();
 homeButton.addEventListener("click", (event) => {
     event.preventDefault();
     home.generateContent();
