f0c21b8c23cecb1a52c49982a1fe7db27d6c2688
OleksandKur <136488587+OleksandrKur@users.noreply.github.com>
2024-10-07 16:56:13 +0300

    Add npm scripts to build, dev and deploy

--- webpack.common.js
+++ webpack.common.js
@@ -2,16 +2,11 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: "development",
   entry: "./src/app.js",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
-  },
-  devtool: "eval-source-map",
-  devServer: {
-    watchFiles: ["./src/template.html"],
   },
   plugins: [
     new HtmlWebpackPlugin({
@@ -25,10 +20,6 @@ module.exports = {
         use: ["style-loader", "css-loader"],
       },
     ],
-  },
-  watchOptions: {
-    aggregateTimeout: 200,
-    poll: 1000,
   },
   resolve: {
     fallback: {
